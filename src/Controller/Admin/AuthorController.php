ddb50a843f70f6872a54b58ebd567ac2cadd7b7b
Yoan DE VRIEZE <devrieze.yoan@gmail.com>
2024-10-05 22:26:47 +0200

    Ajout pagerfanta

--- src/Controller/Admin/AuthorController.php
+++ src/Controller/Admin/AuthorController.php
@@ -6,6 +6,8 @@ use App\Entity\Author;
 use App\Form\AuthorType;
 use App\Repository\AuthorRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Pagerfanta\Doctrine\ORM\QueryAdapter;
+use Pagerfanta\Pagerfanta;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -23,10 +25,26 @@ class AuthorController extends AbstractController
     }
 
     #[Route('', name: 'app_admin_author')]
-    public function author() : Response 
+    public function author(Request $request) : Response 
     {
+        $dates = [];
+
+        if ($request->query->has('start')) {
+            $dates['start'] = $request->query->get('start');
+        }
+
+        if ($request->query->has('end')) {
+            $dates['end'] = $request->query->get('end');
+        }
+
+        $qb = $this->authorRepository->findByDateOfBirth($dates);
+
+        $pagerfanta = new Pagerfanta(
+            new QueryAdapter($qb),
+        );
+
         return $this->render('admin/author/index.html.twig', [
-            'liste_author' => $this->authorRepository->findAll(),
+            'pagerfanta' => $pagerfanta,
         ]);
     }
     

1961a1f50410e327d58e8cb52e5550c115ed03c7
Yoan DE VRIEZE <devrieze.yoan@gmail.com>
2024-10-10 14:23:12 +0200

    Ajout de la contrainte #[Assert\PasswordStrength] à User

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -34,7 +34,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      */
     #[ORM\Column]
     #[Assert\NotBlank()]
-    #[Assert\Length(min: 8, max: 255)]
+    #[Assert\PasswordStrength]
     private ?string $password = null;
 
     #[ORM\Column(length: 255)]

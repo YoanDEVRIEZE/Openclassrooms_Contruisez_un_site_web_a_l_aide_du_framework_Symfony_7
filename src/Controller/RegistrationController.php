5d4e60640f3ed432b6df35d4cc53cdd374effb92
Yoan DE VRIEZE <devrieze.yoan@gmail.com>
2024-10-10 16:25:47 +0200

    modification des routes après connexion et enregistrement utilisateur(s)

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -32,7 +32,7 @@ class RegistrationController extends AbstractController
 
             // do anything else you need here, like send an email
 
-            return $this->redirectToRoute('app_admin_book');
+            return $this->redirectToRoute('app_admin_main');
         }
 
         return $this->render('registration/register.html.twig', [
